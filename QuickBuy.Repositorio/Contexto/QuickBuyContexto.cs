cad55fd7289631d01704f5a44af7f98df37db32d
rodney teixeira <rodney-te@hotmail.com>
2019-07-14 13:43:50 -0300

    Inclusão dos metodos genericos de crud

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -6,13 +6,13 @@ using QuickBuy.Repositorio.Config;
 
 namespace QuickBuy.Repositorio.Contexto
 {
-   public class QuickBuyContexto:DbContext
-   {
-        
-        public DbSet<Usuario>Usuarios { get; set; }
-        public DbSet<Produto>Produtos { get; set; }
-        public DbSet<Pedido>Pedidos { get; set; }
-        public DbSet<ItemPedido>ItemPedidos { get; set; }
+    public class QuickBuyContexto : DbContext
+    {
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
         public DbSet<FormaPagamento> FormaPagamento { get; set; }
 
         public QuickBuyContexto(DbContextOptions options) : base(options)
@@ -26,6 +26,28 @@ namespace QuickBuy.Repositorio.Contexto
             modelBuilder.ApplyConfiguration(new PedidoConfiruration());
             modelBuilder.ApplyConfiguration(new ItemPedidoConfiruration());
             modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+
+            modelBuilder.Entity<FormaPagamento>().HasData(
+      
+            new FormaPagamento()
+            {
+                Id = 1,
+                Nome = "Boleto",
+                Descricao = "Forma de Pagamento Boleto"
+            }
+            ,
+            new FormaPagamento()
+            {
+                Id = 2,
+                Nome = "Cartão de Credito",
+                Descricao = "Forma de Pagamento Cartão de Credito"
+            },
+            new FormaPagamento()
+            {
+                Id = 3,
+                Nome = "Depósito",
+                Descricao = "Forma de Pagamento Deposito"
+            });
 
             base.OnModelCreating(modelBuilder);
         }
--- QuickBuy.Repositorio/Repositorios/ProdutoRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/ProdutoRepositorio.cs
@@ -1,8 +1,13 @@
 ﻿using QuickBuy.Dominio.Entidades;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Contexto;
+
 namespace QuickBuy.Repositorio.Repositorios
 {
     public class ProdutoRepositorio : BaseRepositorio<Produto>, IProdutoRepositorio
     {
+        public ProdutoRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
+        {
+        }
     }
 }
